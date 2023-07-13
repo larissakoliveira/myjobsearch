b795f9c2cc1be7d59857b136329607ca625c3dcd
Larissa <oliveir5uwm@gmail.com>
2023-07-13 18:16:00 -0300

    feat: jobs details implemented

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+import images from "./images";
+import icons from "./icons";
+import { COLORS, FONT, SIZES, SHADOWS, STYLES } from "./theme";
+
+export { images, icons, COLORS, FONT, SIZES, SHADOWS, STYLES };
